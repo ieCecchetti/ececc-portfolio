c12120188d3cf26d218d5536f9b481df03bd371d
k_eCecchetti <enrico.cecchetti@kcitylabs.fr>
2023-09-12 00:19:56 +0200

    fixed links. add project page popup

--- script.js
+++ script.js
@@ -9,11 +9,23 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+$(document).ready(function () {
+    $('body').scrollspy({ target: '#navbar' }); // Adjust the target ID as needed
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+// MODALS
+$(document).ready(function() {
+    // When the modal is shown
+    $('#portfolioModal1').on('show.bs.modal', function (e) {
+        // Get the data-content-url attribute from the clicked element
+        var contentUrl = $(e.relatedTarget).data('content-url');
+        
+        // Set the iframe src attribute to the specified URL
+        $('#modalIframe').attr('src', contentUrl);
+    });
 });
