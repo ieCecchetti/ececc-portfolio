196b582078bfe37aeaf9e546eb6a552bd415edc5
Enrico Cecchetti <enrico.cecchetti@kcitylabs.fr>
2025-01-03 19:40:47 +0100

    add first project

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+$(document).ready(function () {
+  // Smooth scrolling for page links
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr('href')).offset().top,
+        },
+        1500,
+        'easeInOutExpo'
+      );
+    event.preventDefault();
+  });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+  });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+  // Close the responsive menu when a menu item is clicked
+  $('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
+  });
 });
